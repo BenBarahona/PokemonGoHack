93a3690d2f30c729bf93eb62a385bd644f32c45f
Benjamin Barahona <bbaraona@gmail.com>
2016-07-13 15:14:24 -0600

    Initial Commit

---
+++ PokemonHandler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PokemonHandler
+//
+//  Created by Benjamin Barahona on 7/13/16.
+//  Copyright © 2016 Icoms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PokemonHandler/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PokemonHandler
+//
+//  Created by Benjamin Barahona on 7/13/16.
+//  Copyright © 2016 Icoms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
